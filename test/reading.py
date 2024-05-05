f7cebb959902cfd1458072d02a5df9f007bdc764
Jasper van Bourgognie <van.bourgognie@gmail.com>
2024-05-06 01:13:58 +0200

    OSC feedback

--- test/reading.py
+++ test/reading.py
@@ -7,8 +7,13 @@ from statistics import mean
 from termcolor import colored
 from pythonosc import udp_client
 from pythonosc.osc_message_builder import OscMessageBuilder
+import pythonosc.osc_server as osc_server
+import threading
+from pythonosc.dispatcher import Dispatcher
+import queue
 
-# parser = argparse.ArgumentParser()
+import asyncio
+
 packet_format = '<HBBLLLfffB'
 packet_size = struct.calcsize(packet_format)
 
@@ -55,16 +60,59 @@ class Packet():
                 flags & 0x2, time_last_knock,
                 flags & 0x1, time_last_motion, (acc_x, acc_y, acc_z))
 
+class Config():
+    def __init__(self, channel: int, threshold: int):
+        self.channel = channel & 0xff
+        self.threshold = threshold & 0xff
+
+    def bytes(self) -> bytes:
+        payload = [0xE1, 0xBA, self.channel, self.threshold]
+        payload.append(sum(payload) & 0xff)
+        return bytes(payload)
+
+config_q = queue.Queue()
+def foot_handler(address, *args):
+    print(f"{address}: {args} {len(args)}")
+    if(len(args) != 2):
+        print("/foot/threshold needs exactly 2 arguments")
+        return
+
+    if(not isinstance(args[0], int)):
+        print("Argument 0 should be integer")
+        return
+
+    if(not isinstance(args[1], float)):
+        print("Argument 1 should be float")
+        return
+
+    config_q.put(Config(args[0], (int)(args[1] * 255)).bytes())
+
+
+
+dispatcher = Dispatcher()
+dispatcher.map("/foot/threshold", foot_handler)
+
+def start_server(ip, port):
+    print("Starting Server")
+    server = osc_server.ThreadingOSCUDPServer(
+        (ip, port), dispatcher)
+    print("Serving on {}".format(server.server_address))
+    thread = threading.Thread(target=server.serve_forever)
+    thread.start()
+    return thread
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", default="127.0.0.1",
-        help="The ip of the OSC server")
-    parser.add_argument("--port", type=int, default=5005,
-        help="The port the OSC server is listening on")
+        help="The ip to connect to")
+    parser.add_argument("--listen", type=int, default=5008,
+        help="The port to listen on")
+    parser.add_argument("--send", type=int, default=5005,
+        help="The port to send to")
     args = parser.parse_args()
 
-    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    server_thread = start_server(args.ip, args.listen)
+    client = udp_client.SimpleUDPClient(args.ip, args.send)
 
     port = "/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0"
     ser = serial.Serial(port, 115200)
@@ -73,26 +121,40 @@ if __name__ == '__main__':
 
     deltas = collections.deque([1], maxlen=100)
     last_time = 0
-    while(True):
-        packet = Packet.from_bytes(ser.read(packet_size))
+    config = Config(1, 5)
+    try:
+        while(True):
+            if not config_q.empty():
+                ser.write(config_q.get())
 
-        if last_time != 0:
-            deltas.append(packet.time - last_time)
-        last_time = packet.time
+            packet = Packet.from_bytes(ser.read(packet_size))
 
-        prefix = f"/foot/{packet.id}"
-        message = {
-            "/time": packet.time,
-            "/knock": packet.knock,
-            "/knock_time": packet.knock_time,
+            if last_time != 0:
+                deltas.append(packet.time - last_time)
+            last_time = packet.time
 
-            "/motion": packet.motion,
-            "/motion_time": packet.motion,
+            prefix = f"/foot/{packet.id}"
+            message = {
+                "/time": packet.time,
+                "/knock": packet.knock,
+                "/knock_time": packet.knock_time,
 
-        }
-        for k,v in message.items():
-            client.send_message(prefix + k, v)
-        msg_acc = OscMessageBuilder(prefix + "/acc")
-        [msg_acc.add_arg(x) for x in packet.acc]
-        client.send(msg_acc.build())
-        print(colored(f"{1e3/mean(deltas):5.2f}Hz", 'yellow') + colored(str(packet), 'red' if packet.motion else 'green' if packet.knock else 'white'))
+                "/motion": packet.motion,
+                "/motion_time": packet.motion,
+
+            }
+            for k,v in message.items():
+                client.send_message(prefix + k, v)
+            msg_acc = OscMessageBuilder(prefix + "/acc")
+            [msg_acc.add_arg(x) for x in packet.acc]
+            client.send(msg_acc.build())
+            # if packet.motion or packet.knock:
+            print(colored(f"{1e3/mean(deltas):5.2f}Hz", 'yellow') + colored(str(packet), 'red' if packet.motion else 'green' if packet.knock else 'white'))
+            # if packet.motion:
+            #     client.send_message("/cue/3/start", 1)
+            # if packet.knock:
+            #     client.send_message("/cue/4/start", 1)
+    except KeyboardInterrupt:
+        print("CTRL-C hit, ending")
+
+    server_thread.stop()
