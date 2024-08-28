fd7381938a5c6b7cd94274253e79421d0a7b5a27
Jasper van Bourgognie <van.bourgognie@gmail.com>
2024-08-29 01:43:11 +0200

    config update by OSC

--- scripts/osc_server.py
+++ scripts/osc_server.py
@@ -8,8 +8,6 @@ from pythonosc.osc_server import ThreadingOSCUDPServer
 from config import Config
 
 class OscServer(QRunnable):
-    config_changed = pyqtSignal()
-
     def __init__(self, config: Config):
         super(OscServer, self).__init__()
         self.config = config
@@ -22,7 +20,6 @@ class OscServer(QRunnable):
         self.dispatcher = Dispatcher()
         self.dispatcher.map("/tracker/*", self.handler)
         self.dispatcher.set_default_handler(self.default_handler)
-        # self.signals.result.connect(self._do_stop_server)
 
         while(self.running):
             print(f"Starting OSC receiver on port {self.config.osc_receive_port}")
@@ -44,6 +41,31 @@ class OscServer(QRunnable):
     def stop(self):
         self.running = False
         self.server.shutdown()
+
+    def update_list(self, attribute, *args):
+        if not isinstance(args[0], int):
+            print(f"First argument should be an integer, not {type(args[0])}")
+            return
+
+        if args[0] >= len(attribute):
+            print(f"Index {args[0]} out of range [0,{len(attribute)}]")
+            return
+
+        idx = args[0]
+        if isinstance(attribute[idx], list):
+            self.update_list(attribute[idx], *args[1:])
+            return
+
+        if len(args) != 2:
+            print(f"I need 2 arguments to update a list value - not {args}")
+            return
+
+        if not isinstance(args[1], type(attribute[idx])):
+            print(f"Expecting second argument {args[1]} to be of type {type(attribute[idx])}, not {type(args[1])}")
+            return
+
+        attribute[idx] = args[1]
+
 
     def handler(self, address, *args):
         try:
@@ -73,36 +95,20 @@ class OscServer(QRunnable):
 
         attribute = getattr(tracker, attribute_name)
         if isinstance(attribute, list):
-            if len(args) != 2:
-                print(f"I need 2 arguments to update a list value - not {args}")
-                return
-            if not isinstance(args[0], int):
-                print(f"First argument should be an integer, not {type(args[0])}")
-                return
-            if args[0] >= len(attribute):
-                print(f"Index {args[0]} out of range [0,{len(attribute)}]")
-                return
+            self.update_list(attribute, *args)
 
-            idx = args[0]
-            if not isinstance(args[1], type(attribute[idx])):
-                print(f"Expecting second argument {args[1]} to be of type {type(attribute[idx])}, not {type(args[1])}")
-                return
-
-            print(f"Updating tracker[{tracker_idx}].{attribute_name}[{idx}] to {args[1]}")
-            attribute[idx] = args[1]
         else:
             if len(args) != 1:
-                print(f"I need exactly arguments to update {attribute_name} - not {args}")
+                print(f"I need exactly 1 argument to update {attribute_name} - not {args}")
                 return
 
             if not isinstance(args[0], type(attribute)):
                 print(f"Expecting argument {args[0]} to be of type {type(attribute)}, not {type(args[0])}")
                 return
 
-            print(f"Updating tracker[{tracker_idx}].{attribute_name} to {args[0]}")
             setattr(tracker, attribute_name, args[0])
 
-        self.config_changed.emit()
+        self.signals.result.emit(self.config)
 
     def default_handler(self, address, *arg):
         print(f"Ignoring OSC message at {address}")
