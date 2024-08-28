eea8dc77154768524e187bbcef9293be4d505306
Jasper van Bourgognie <van.bourgognie@gmail.com>
2024-08-29 00:34:01 +0200

    osc server config update handler

--- scripts/osc_server.py
+++ scripts/osc_server.py
@@ -8,6 +8,8 @@ from pythonosc.osc_server import ThreadingOSCUDPServer
 from config import Config
 
 class OscServer(QRunnable):
+    config_changed = pyqtSignal()
+
     def __init__(self, config: Config):
         super(OscServer, self).__init__()
         self.config = config
@@ -43,8 +45,64 @@ class OscServer(QRunnable):
         self.running = False
         self.server.shutdown()
 
-    def handler(self, address, *arg):
-        print(f"{address}: {arg}")
+    def handler(self, address, *args):
+        try:
+            (_, _, tracker_pointer, attribute_name) = address.strip().split("/")
+        except:
+            print(f"Bad structure of address {address}")
+            return
+
+        if str(int(tracker_pointer)) == tracker_pointer:
+            tracker_idx = int(tracker_pointer)
+            if tracker_idx > len(self.config.trackers):
+                print(f"OSC Server: tracker index {tracker_idx} out of range")
+                return
+        else:
+            for (i, t) in enumerate(self.config.trackers):
+                if t.alias == i:
+                    tracker_idx = i
+                    break
+            else:
+                print(f"OSC Server: tracker with alias {tracker_pointer} not found")
+                return
+
+        tracker = self.config.trackers[tracker_idx]
+        if not hasattr(tracker, attribute_name):
+            print(f"Unknown tracker config {attribute_name}")
+            return
+
+        attribute = getattr(tracker, attribute_name)
+        if isinstance(attribute, list):
+            if len(args) != 2:
+                print(f"I need 2 arguments to update a list value - not {args}")
+                return
+            if not isinstance(args[0], int):
+                print(f"First argument should be an integer, not {type(args[0])}")
+                return
+            if args[0] >= len(attribute):
+                print(f"Index {args[0]} out of range [0,{len(attribute)}]")
+                return
+
+            idx = args[0]
+            if not isinstance(args[1], type(attribute[idx])):
+                print(f"Expecting second argument {args[1]} to be of type {type(attribute[idx])}, not {type(args[1])}")
+                return
+
+            print(f"Updating tracker[{tracker_idx}].{attribute_name}[{idx}] to {args[1]}")
+            attribute[idx] = args[1]
+        else:
+            if len(args) != 1:
+                print(f"I need exactly arguments to update {attribute_name} - not {args}")
+                return
+
+            if not isinstance(args[0], type(attribute)):
+                print(f"Expecting argument {args[0]} to be of type {type(attribute)}, not {type(args[0])}")
+                return
+
+            print(f"Updating tracker[{tracker_idx}].{attribute_name} to {args[0]}")
+            setattr(tracker, attribute_name, args[0])
+
+        self.config_changed.emit()
 
     def default_handler(self, address, *arg):
-        print(f"DEFAULT {address}: {arg}")
+        print(f"Ignoring OSC message at {address}")
